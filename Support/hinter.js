74877c87c3f098483ecfc06b64e9b78d1a28362f
Arne Martin Aurlien <am@aurlien.net>
2014-01-06 12:35:10 +0100

    Add support for error messages from source transforms

--- Support/hinter.js
+++ Support/hinter.js
@@ -32,17 +32,40 @@ function hint(source, cb) {
         var t = spawn(process.env.TM_HINTMATE_TRANSFORM, [], {
             cwd: cwd
         });
+        var stderr = '';
+        t.stderr.on('data', function(data) {
+            stderr += data.toString() + '\n';
+        });
+        t.on('close', function(code) {
+            if (code !== 0) {
+                if (process.env.TM_HINTMATE_TRANSFORM_ERROR) {
+                    var error = stderr.match(new RegExp(process.env.TM_HINTMATE_TRANSFORM_ERROR));
+                    if (error && !called) {
+                        called = true;
+                        cb(error[0]);
+                        return;
+                    }
+                }
+                if (!called) {
+                    called = true;
+                    cb('Error during transform');
+                }
+            }
+        });
         t.stdout.pipe(h.stdin);
         t.stdin.end(prepareSource(source));
     } else {
         h.stdin.end(prepareSource(source));
     }
     h.stdout.on('data', function(data) {
-        called = true;
-        cb(parse(data.toString()));
+        if (!called) {
+            called = true;
+            cb(parse(data.toString()));
+        }
     });
     h.on('close', function() {
         if (!called) {
+            called = true;
             cb([]);
         }
     });
@@ -89,9 +112,13 @@ function formatSimple(errors, limit) {
     if (limit === undefined) {
         limit = 5;
     }
-    return errors.slice(0, limit).map(function(error) {
-        return 'Line ' + error.line + ', col ' + error.col + ': ' + error.message;
-    }).join('\n') + (errors.length > limit ? '\nFound ' + errors.length + ' errors total.' : '');
+    if (typeof errors === 'string') {
+        return errors;
+    } else {
+        return errors.slice(0, limit).map(function(error) {
+            return 'Line ' + error.line + ', col ' + error.col + ': ' + error.message;
+        }).join('\n') + (errors.length > limit ? '\nFound ' + errors.length + ' errors total.' : '');
+    }
 }
 
 function wrapHtml(content) {
@@ -101,7 +128,7 @@ function wrapHtml(content) {
     '<meta charset="UTF-8" />' +
     '<title>Hintmate</title>' +
     '<style type="text/css">' +
-    'body { font-family: helvetica, sans-serif; background-color: #222; color: #ddd }' +
+    'body { background-color: #222; color: #ddd }' +
     'a, a:visited, a:active { color: white }' +
     'a { display: block; margin: 5px; padding: 6px 4px; border: 1px solid #444; border-radius: 5px; text-decoration: none; }' +
     '.dim { color: #999 }' +
@@ -120,6 +147,8 @@ function wrapHtml(content) {
 function formatHtml(errors) {
     if (errors.length === 0) {
         return wrapHtml('<p>No errors!</p>');
+    } else if (typeof errors === 'string') {
+        return wrapHtml('<p>' + errors + '</p>');
     } else {
         return wrapHtml('<ul>' + errors.reduce(function(html, error) {
             return html + '<li><a href="txmt://open?url=file://' + process.env.TM_FILEPATH + '&line=' + error.line + '&column=' + error.col + '"><span class="dim">Line ' + error.line + ', col ' + error.col + ':</span> ' + error.message + '</a></li>';
